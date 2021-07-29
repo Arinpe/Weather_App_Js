03e5d252b12451c2b91b2a1dc953ade658572659
Arinpe <dehtolah@gmail.com>
2021-07-29 14:56:07 +0100

    Worked on the await function

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ weatherForm.addEventListener('submit', async (event) => {
   const unit = formData.get('unit');
   const endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}&units=${unit}`;
 
-  await fetch(endpoint)
+  try {
+    await fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
       const { name } = data;
@@ -35,4 +36,31 @@ weatherForm.addEventListener('submit', async (event) => {
       document.querySelector('.weather').classList.remove('Loading');
       document.body.style.backgroundImage = `url('http://source.unsplash.com/1600x900/?${name} ')`;
     });
+    } catch(e) {
+      console.log(e.message)
+    }
+  // await fetch(endpoint)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     const { name } = data;
+  //     const { description } = data.weather[0];
+  //     const { temp, humidity } = data.main;
+  //     const { speed } = data.wind;
+  //     const unitSymbol = unit === 'metric' ? '°C' : '°F';
+  //     const windSpeed = unit === 'metric' ? 'km/h' : 'm/s';
+
+    //   document.querySelector('.city').innerText = `Weather in ${name}`;
+    //   document.querySelector(
+    //     '.description',
+    //   ).innerText = `Cloudy: ${description}`;
+    //   document.querySelector('.temp').innerText = temp + unitSymbol;
+    //   document.querySelector(
+    //     '.humidity',
+    //   ).innerText = `Humidity: ${humidity}%  `;
+    //   document.querySelector(
+    //     '.wind',
+    //   ).innerText = `Wind speed ${speed}${windSpeed}`;
+    //   document.querySelector('.weather').classList.remove('Loading');
+    //   document.body.style.backgroundImage = `url('http://source.unsplash.com/1600x900/?${name} ')`;
+    // });
 });
