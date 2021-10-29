175c40acf81ba997070900b9aa801b81b3d9d77d
Arinpe <dehtolah@gmail.com>
2021-10-29 15:32:56 +0100

    Update App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,20 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
+  include ActionController::Serialization
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
+  rescue_from ActiveRecord::RecordNotUnique, with: :not_unique
 
-  def encode_token(payload)
-    JWT.encode(payload, 'yourSecret')
+  private
+
+  def not_found(errors)
+    render json: errors, status: :not_found
   end
 
-  def auth_header
-    # { Authorization: 'Bearer <token>' }
-    request.headers['Authorization']
+  def invalid(errors)
+    render json: errors, status: :unprocessable_entity
   end
 
-  def decoded_token
-    if auth_header
-      token = auth_header.split(' ')[1]
-      # header: { 'Authorization': 'Bearer <token>' }
-      begin
-        JWT.decode(token, 'yourSecret', true, algorithm: 'HS256')
-      rescue JWT::DecodeError
-        nil
-      end
-    end
-  end
-
-  def logged_in_user
-    if decoded_token
-      user_id = decoded_token[0]['user_id']
-      @user = User.find_by(id: user_id)
-    end
-  end
-
-  def logged_in?
-    !!logged_in_user
-  end
-
-  def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  def not_unique(errors)
+    render json: errors, status: :internal_server_error
   end
 end
