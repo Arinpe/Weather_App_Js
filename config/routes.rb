2fd9d16739498a591001b15a26375d7a23e9ec73
Arinpe <dehtolah@gmail.com>
2021-10-29 15:34:00 +0100

    Update bin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resource :users, only: [:create]
-  post "/login", to: "users#login"
-  get "/auto_login", to: "users#auto_login"
+  root to: "api/v1/users#index"
+  scope module: 'api' do
+    namespace :v1 do
+      resources :users, only: %i[create show]
+      resources :cars, only: %i[index show]
+      post 'appointments', to: 'users#new_appointment'
+    end
+  end
 end
